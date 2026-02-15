759b65be1af5b21c2031928cb35cc34dc9df3151
Nathan Rodrigues <nathanrodriguesvieira99@gmail.com>
2026-02-14 23:13:47 -0300

    feat: add custom render utility for testing with React Testing Library

---
+++ src/test/utils/custom-render.tsx
@@ -0,0 +1,12 @@
+import type { ReactElement } from 'react';
+import { render, RenderOptions } from '@testing-library/react';
+
+export function CustomRender(
+  ui: ReactElement,
+  options?: Omit<RenderOptions, 'wrapper'>
+) {
+  return render(ui, { ...options });
+}
+
+export * from '@testing-library/react';
+export { CustomRender as render };
