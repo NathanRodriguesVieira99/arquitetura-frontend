bc2e5ce72fd01905df0f03cb207945bc372e5323
Nathan Rodrigues <nathanrodriguesvieira99@gmail.com>
2026-02-17 18:30:37 -0300

    feat: install and setup storybook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,10 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+
+import storybook from 'eslint-plugin-storybook';
+import react from 'eslint-plugin-react';
+
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -8,11 +12,21 @@ const eslintConfig = defineConfig([
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
+  ...storybook.configs['flat/recommended'],
+  {
+    plugins: { react },
+    settings: {
+      react: { version: 'detect' },
+    },
+    rules: {
+      'react/jsx-key': 'error',
+    },
+  },
 ]);
 
 export default eslintConfig;
